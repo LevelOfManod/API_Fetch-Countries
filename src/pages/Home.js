979b2dde2413f7b86f3fb95b30601f08a287de3a
mathieu <vanderlyndenmathieu@gmail.com>
2023-10-11 00:32:26 +0200

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Countries from '../components/Countries';
+
+const Home = () => {
+    return (
+        <>
+            <Countries />
+        </>
+    );
+};
+
+export default Home;
